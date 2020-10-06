783aa9833ca4e0abd43e7ae55d2f4718bc4038b4
Sourav <sourav.iit.nstu@gmail.com>
2020-10-06 19:07:50 +0600

    hello world

---
+++ Hello world/Hello world/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace Hello_world
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            Console.ReadLine();
+        }
+    }
+}
