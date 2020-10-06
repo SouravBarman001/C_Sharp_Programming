7a8a447cf9547b28f63d16ca7c05b0c11c4b3c66
SouravBarman001 <61220968+SouravBarman001@users.noreply.github.com>
2020-10-06 19:39:55 +0600

    two number swap program

---
+++ CSharp swapping two variables/CSharp swapping two variables/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace CSharp_swapping_two_variables
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            int number1, number2, temp;
+
+            Console.WriteLine("Enter 1st number:");
+            number1 = int.Parse(Console.ReadLine());
+
+
+            Console.WriteLine("Enter 2nd number:");
+            number2 = int.Parse(Console.ReadLine());
+
+
+            temp = number1;
+            number1 = number2;
+            number2 = temp;
+
+            Console.WriteLine("1st number is :"+number1);
+            Console.WriteLine("2nd number is:"+number2);
+
+
+
+
+        }
+    }
+}
